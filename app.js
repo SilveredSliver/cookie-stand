a13938c5b10ef1ec177d3bd40099ce3cda6c75db
AaronBruce <aaron.bruce@comcast.net>
2017-04-10 21:07:46 -0700

    First phase of the cookie-stand lab

---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var cookieList = {
+  title: 'Cookie Stand',
+  types: [
+    'Coho Cookies',
+    'King Cookies',
+    'Sockeye Cookies',
+    'Chum Cookies',
+  ],
+  employeeList: [
+    'Larry',
+    'Mo',
+    'Curly',
+  ],
+  address: '5116 Kenilworth PL NE, Seattle, WA',
+  businessHours: '9:00 AM - 5:30 PM',
+};
+
+var heading = document.getElementById('title');
+heading.textContent = cookieList.title;
+var ingredientsUl = document.getElementById('cookieTypes');
+
+var ingredientLi;
+
+
+for (var i = 0; i < cookieList.types.length; i++) {
+  console.log('cookieList.types of i', cookieList.types[i]);
+  ingredientLi = document.createElement('li');
+
+  ingredientLi.textContent = cookieList.types[i];
+  ingredientsUl.appendChild(ingredientLi);
+}
+
+
+var stooges = document.getElementById('employeeList');
+
+  for (var i = 0; i < cookieList.employeeList.length; i++) {
+    console.log('cookieList.employeeList', cookieList.employeeList);
+    ingredientLi = document.createElement('li');
+
+    ingredientLi.textContent = cookieList.employeeList[i];
+    stooges.appendChild(ingredientLi);
+  }
+var locationStore = document.getElementById('address');
+ingredientLi = document.createElement('li');
+ingredientLi.textContent = cookieList.address;
+
+locationStore.appendChild(ingredientLi);
+
+
+var storeHours = document.getElementById('businessHours');
+ingredientLi = document.createElement('li');
+ingredientLi.textContent = cookieList.businessHours;
+
+storeHours.appendChild(ingredientLi);
